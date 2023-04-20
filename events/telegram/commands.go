b571088085bc7523eaef36ebb1b88a21079af5ab
nnnlik <makhmudov_ra@vk.com>
2023-04-20 16:23:43 +0300

    [ADD] commands

--- events/telegram/commands.go
+++ events/telegram/commands.go
@@ -1,37 +1,100 @@
 package telegram
 
 import (
+	"errors"
+	"link-collector-bot/lib/e"
+	"link-collector-bot/storage"
 	"log"
+	"net/url"
 	"strings"
 )
 
 const (
-	RndCmd = "/rnd"
-	HelpCmd = "/help"
+	RndCmd   = "/rnd"
+	HelpCmd  = "/help"
 	StartCmd = "/start"
 )
 
 func (p *Processor) doCmd(text string, chatID int, username string) error {
 	text = strings.TrimSpace(text)
 
-	log.Printf("got new command '%s' from '%s'", text, username)
+	log.Printf("got new command '%s' from '%s", text, username)
 
 	if isAddCmd(text) {
-		
+		return p.savePage(chatID, text, username)
 	}
 
 	switch text {
 	case RndCmd:
+		return p.sendRandom(chatID, username)
 	case HelpCmd:
+		return p.sendHelp(chatID)
 	case StartCmd:
+		return p.sendHello(chatID)
 	default:
+		return p.tg.SendMessage(chatID, msgUnknownCommand)
 	}
+}
+
+func (p *Processor) savePage(chatID int, pageURL string, username string) (err error) {
+	defer func() { err = e.WrapIsErr("can't do command: save page", err) }()
+
+	page := &storage.Page{
+		URL:      pageURL,
+		UserName: username,
+	}
+
+	isExists, err := p.storage.IsExists(page)
+	if err != nil {
+		return err
+	}
+	if isExists {
+		return p.tg.SendMessage(chatID, msgAlreadyExists)
+	}
+
+	if err := p.storage.Save(page); err != nil {
+		return err
+	}
+
+	if err := p.tg.SendMessage(chatID, msgSaved); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *Processor) sendRandom(chatID int, username string) (err error) {
+	defer func() { err = e.WrapIsErr("can't do command: can't send random", err) }()
+
+	page, err := p.storage.PickRandom(username)
+	if err != nil && !errors.Is(err, e.ErrNoSavedPages) {
+		return err
+	}
+	if errors.Is(err, e.ErrNoSavedPages) {
+		return p.tg.SendMessage(chatID, msgNoSavedPages)
+	}
+
+	if err := p.tg.SendMessage(chatID, page.URL); err != nil {
+		return err
+	}
+
+	return p.storage.Remove(page)
+}
+
+func (p *Processor) sendHelp(chatID int) error {
+	return p.tg.SendMessage(chatID, msgHelp)
+}
+
+func (p *Processor) sendHello(chatID int) error {
+	return p.tg.SendMessage(chatID, msgGreeting)
 }
 
 func isAddCmd(text string) bool {
 	return isURL(text)
 }
-// TODO
+
 func isURL(text string) bool {
-	return true
+	u, err := url.Parse(text)
+
+	return err == nil && u.Host != ""
 }
